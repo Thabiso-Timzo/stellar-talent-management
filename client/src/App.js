c3b079a67046b6668bd94b208d6982342d4d21e8
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-04-18 16:54:26 +0200

    files Modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from 'react'; 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import NavBar from './navigation/NavBar';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Gallery from './pages/Gallery/Gallery';
+import Home from './pages/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      Thabiso Hlatshwayo
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={Home} exact>
+          <Home />
+        </Route>
+        <Route path="/about" component={About} exact>
+          <About />
+        </Route>
+        <Route path="/gallery" component={Gallery} exact>
+          <Gallery />
+        </Route>
+        <Route path="/contact" component={Contact} exact>
+          <Contact />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
