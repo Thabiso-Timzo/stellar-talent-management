2b85b393f6e4e164cef37b5312acb65f130b58c9
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-04-21 22:09:00 +0200

    Added contact us page

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
 
+import './Contact.css'
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contactUs">
+      <div className="title">
+        <h2>Get in touch</h2>
+      </div>
+      <div className="box">
+        <div className="contact-div form">
+          <h3>Send a message</h3>
+          <form>
+            <div className="formBox">
+              <div className="row50">
+                <div className="inputBox">
+                  <span>First Name</span>
+                  <input type="text" name="" id="" />
+                </div>
+                <div className="inputBox">
+                  <span>Last Name</span>
+                  <input type="text" name="" id="" />
+                </div>
+              </div>
+
+              <div className="row50">
+                <div className="inputBox">
+                  <span>Email</span>
+                  <input type="text" name="" id="" />
+                </div>
+                <div className="inputBox">
+                  <span>Cellphone Number</span>
+                  <input type="text" name="" id="" />
+                </div>
+              </div>
+
+              <div className="row100">
+                <div className="inputBox">
+                  <span>Message</span>
+                  <textarea name="" id="" cols="30" rows="10" placeholder="Write your message here..."></textarea>
+                </div>
+              </div>
+
+              <div className="row100">
+                <div className="inputBox">
+                  <button>Send</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="contact-div info"></div>
+        <div className="contact-div map"></div>
+      </div>
+    </div>
   )
 }
 
