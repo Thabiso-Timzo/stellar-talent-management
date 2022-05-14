72060870f2170208cdb83a4039fc3cbca1a04ccd
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-05-14 20:01:45 +0200

    Modified App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,35 @@
 import React from 'react'; 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import NavBar from './navigation/NavBar';
+import Navbar from './navigation/Navbar';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
 import Gallery from './pages/Gallery/Gallery';
+import Services from './pages/services/Services';
 import Home from './pages/Home/Home';
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path="/" component={Home} exact>
-          <Home />
-        </Route>
-        <Route path="/about" component={About} exact>
-          <About />
-        </Route>
-        <Route path="/gallery" component={Gallery} exact>
-          <Gallery />
-        </Route>
-        <Route path="/contact" component={Contact} exact>
-          <Contact />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' component={Home} exact>
+            <Home />
+          </Route>
+          <Route path='/about' component={About} exact>
+            <About />
+          </Route>
+          <Route path='/services' component={Services} exact>
+            <Services />
+          </Route>
+          <Route path='/gallery' component={Gallery} exact>
+            <Gallery />
+          </Route>
+          <Route path='/contact' component={Contact} exact>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
   );
 }
 
