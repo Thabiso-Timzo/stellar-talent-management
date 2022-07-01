9a63a42aae5c255a02a0ac76bbf2ee479b9c93f6
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-07-01 15:34:15 +0200

    Modified index file

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
 
 const emailRoutes = require('./routes/EmailRoutes');
 
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('tiny'));
 
-app.use('/api/email', cors(), emailRoutes); 
+app.use('/api/email', emailRoutes); 
 
 app.listen(port, () => {
-    console.log(`Server is up and running on port: ${port}`)
+    console.log(`Server is up and running on port: ${port}`);
 })
