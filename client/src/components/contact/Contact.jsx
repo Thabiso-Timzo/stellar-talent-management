c16a22c1a4e700377c455db5dbb15e9a0634105b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-07-04 17:22:18 +0200

    Modified contact files

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -4,33 +4,65 @@ import axios from 'axios'
 import './Contact.css'
 
 const Contact = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [sent, setSent] = useState(false)
+    const initialValues = { 
+        name: '', 
+        email: '', 
+        message: '', 
+        sent: false 
+    }
+    const [formValues, setFormValues] = useState(initialValues)
+    const [formErrors, setFormErrors] = useState({})
 
-    const formSubmit = (e) => {
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormValues({...formValues, [name]: value})
+        console.log(formValues)
+    }
+    
+    const formSubmit = (e) => { 
         e.preventDefault()
+        setFormErrors(validate(formValues))
 
         let data = {
-            name: name,
-            email: email,
-            message: message,
+            name: formValues.name,
+            email: formValues.email,
+            message: formValues.message,
         }
 
         axios.post('/api/email', data)
         .then(response => {
-            setSent(true)
+             setFormValues({ sent: true });
         }).catch(() => {
             console.log('Message not sent!')
         })
+    }
+
+    const validate = (values) => {
+        const errors = {}
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+
+        if (!values.name) {
+            errors.name = "Name is required!"
+        }
+
+        if (!values.email) {
+            errors.email = "Email is required!"
+        } else if (!regex.test(values.email)) {
+            errors.email = "This is not a valid email format"
+        }
+
+        if (!values.message) {
+            errors.message = "Message is required!"
+        }
+
+        return errors
     }
 
   return (
     <div className='contact'>
         <div className="container">
             <div className="form-container">
-                <form onClick={formSubmit}> 
+                <form> 
                     <h1><span>Contact</span> Us</h1>
                     <div>
                         <label>Name</label>
@@ -39,10 +71,11 @@ const Contact = () => {
                             type="text" 
                             placeholder='Enter your name'
                             name='name'
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
+                            value={formValues.name}
+                            onChange={handleChange}
                         />
                     </div>
+                    <p>{ formErrors.name }</p>
                     <div>
                         <label>Email</label>
                         <input 
@@ -50,10 +83,11 @@ const Contact = () => {
                             type="email" 
                             placeholder='Enter your email'
                             name='email'
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            value={formValues.email}
+                            onChange={handleChange}
                         />
                     </div>
+                    <p>{ formErrors.email }</p>
                     <div>
                         <label>Message</label>
                         <textarea 
@@ -61,12 +95,13 @@ const Contact = () => {
                             rows='10' 
                             placeholder='Enter your message'
                             name='message'
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
+                            value={formValues.message}
+                            onChange={handleChange}
                         />
                     </div>
-                    {sent ? <div className="sentMessage">Message sent</div> : null}
-                    <button>Submit</button>
+                    <p>{ formErrors.message }</p>
+                    {formValues.sent ? <div className="sentMessage">Message sent</div> : null}
+                    <button onClick={formSubmit}>Submit</button>
                 </form>
             </div>
         </div>
