da4751143c2f5d81164ec39d5966f6c645d2c39f
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-07-09 15:25:29 +0200

    Added config/index.jsx file

---
+++ client/src/config/index.js
@@ -0,0 +1,15 @@
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0.1/8 is considered localhost for IPv4
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?)){3}$/
+    )
+)
+
+export const API_URL = isLocalhost ? (
+    "http://localhost:5000/api/email"
+) : (
+    "https://startalentmanagmentapi.org.za/api/email"
+)
